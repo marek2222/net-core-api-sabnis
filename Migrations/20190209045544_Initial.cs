81b82a2736ae6c92cf3a51308eb193cf781723b8
marek2222 <marekmusial2@o2.pl>
2019-02-09 06:04:12 +0100

    api-sabnis: 1_6:    dotnet ef migrations add Initial;  dotnet ef database update

---
+++ Migrations/20190209045544_Initial.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace net_core_api_sabnis.Migrations
+{
+  public partial class Initial : Migration
+  {
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.CreateTable(
+        name: "Books",
+        columns: table => new
+        {
+          Id = table.Column<int>(nullable: false),
+          BookTitle = table.Column<string>(maxLength: 50, nullable: false),
+          AuthorName = table.Column<string>(maxLength: 100, nullable: false),
+          Publisher = table.Column<string>(maxLength: 100, nullable: false),
+          Genre = table.Column<string>(nullable: true),
+          Price = table.Column<int>(nullable: false)
+        },
+        constraints: table =>
+        {
+          table.PrimaryKey("PK_Books", x => x.Id);
+        });
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.DropTable( name: "Books");
+    }
+  }
+}
